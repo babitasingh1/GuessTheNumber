12919b747cfcfa313709239a4652f70f10193555
COOL\babita.singh <babita.singh1@gmail.com>
2021-12-29 09:55:56 +0100

    create guess the number game basics

---
+++ GuessTheNumber.js
@@ -0,0 +1,44 @@
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+const userInput = document.querySelector("input");
+const submitButton = document.getElementById("submitBtn");
+const restartButton = document.getElementById("restartBtn");
+const message = document.querySelector("p");
+const form = document.querySelector("form");
+let counter = 0;
+
+const validateInput = (e) => {
+  e.preventDefault();
+  message.textContent = "";
+  if (counter == 10) {
+    message.textContent = "You did not guess the no.";
+    message.style.padding = 4;
+    message.style.backgroundColor = "red";
+    form.reset();
+    restartButton.hidden = false;
+    submitButton.disabled = true;
+    return;
+  }
+  console.log(randomNumber);
+  console.log(userInput.value);
+  if (userInput.value > randomNumber) {
+    message.textContent = "input is too higher than the random";
+    message.style.padding = 4;
+    message.style.backgroundColor = "red";
+  } else if (userInput.value == randomNumber) {
+    message.textContent = "input is equal to the no";
+    message.style.padding = 4;
+    message.style.backgroundColor = "green";
+    form.reset();
+    restartButton.hidden = false;
+    submitButton.disabled = true;
+
+    return;
+  } else if (userInput.value < randomNumber) {
+    message.textContent = "input is lower than the no";
+    message.style.padding = 4;
+    message.style.backgroundColor = "red";
+  }
+
+  counter += 1;
+};
+submitButton.addEventListener("click", validateInput);
